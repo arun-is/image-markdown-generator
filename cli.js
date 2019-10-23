4df89f14697bd74f70a5441460422f3c79fc7794
Arun Venkatesan <dallasarun@gmail.com>
2019-10-22 21:30:59 -0700

    return images in correct order

--- cli.js
+++ cli.js
@@ -19,14 +19,28 @@ const run = async () => {
 
   const files = await readdirAsync(directory)
 
-  await files.sort().forEach(async fileName => {
-    const m = await getExif(path.join(directory, fileName), true)
+  const markdownStrings = {}
+
+  const promises = await files.map(async fileName => {
+    if (fileName === ".DS_Store") return
+    let m
+    try {
+      m = await getExif(path.join(directory, fileName), true)
+    } catch (e) {
+      console.error(`problem with ${fileName}`)
+    }
     const shutterSpeed = exposureTimeToHuman(m.exif.ExposureTime)
 
     const imageString = `![](${fileName}) <span class="f5 db tr o-40 nb3">Leica Q · f/${m.exif.FNumber} · ${shutterSpeed} · ISO ${m.exif.ISO}</span>\n\n<br><br>\n\n`
 
-    console.log(imageString)
+    markdownStrings[fileName] = imageString
+
+    return m
   })
+
+  await Promise.all(promises)
+
+  files.forEach(fileName => console.log(markdownStrings[fileName]))
 }
 
 run()
